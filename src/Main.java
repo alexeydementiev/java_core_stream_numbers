c71c44e0e6ce783f327daad8a650be234ffb4f39
alexeydementiev <adementiev@flyxo.com>
2022-04-03 22:01:55 +0300

    Realization for + stream

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,25 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+        List<Integer> positiveNumbers = new ArrayList<>();
+        for (int num : intList) {
+            if (num > 0) {
+                if (num % 2 == 0) {
+                    positiveNumbers.add(num);
+
+                }
+            }
+        }
+        Collections.sort(positiveNumbers);
+        System.out.println("Выведем массив созданный при помощи перебора циклом + условия:");
+        System.out.println(positiveNumbers);
+
+        StreamMain.main(args);
     }
 }
